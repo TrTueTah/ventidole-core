1dd8d5269f067a9321003a0b1bb36680c95e1617
Tanh Tran <119776880+TrTueTah@users.noreply.github.com>
2025-11-09 18:10:54 +0700

    add linux:

--- src/db/prisma/client.ts
+++ src/db/prisma/client.ts
@@ -42,6 +42,10 @@ export { Prisma }
 path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node")
 path.join(process.cwd(), "src/db/prisma/libquery_engine-darwin-arm64.dylib.node")
 
+// file annotations for bundling tools to include these files
+path.join(__dirname, "libquery_engine-linux-musl-openssl-3.0.x.so.node")
+path.join(process.cwd(), "src/db/prisma/libquery_engine-linux-musl-openssl-3.0.x.so.node")
+
 /**
  * Model Account
  * 
