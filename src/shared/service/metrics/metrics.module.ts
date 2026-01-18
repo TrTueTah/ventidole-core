586b94347c3a13401c63d326f54303f1f10958f7
Tanh Tran <119776880+TrTueTah@users.noreply.github.com>
2026-01-18 14:54:52 +0700

    fix prometheus

--- src/shared/service/metrics/metrics.module.ts
+++ src/shared/service/metrics/metrics.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { ENVIRONMENT } from '@core/config/env.config';
+import { MetricsInterceptor } from '@core/interceptor/metrics.interceptor';
 import { metricsProviders } from './metrics.provider';
 
 @Module({
@@ -19,7 +20,7 @@ import { metricsProviders } from './metrics.provider';
       },
     }),
   ],
-  providers: [...metricsProviders],
-  exports: [PrometheusModule, ...metricsProviders],
+  providers: [...metricsProviders, MetricsInterceptor],
+  exports: [PrometheusModule, ...metricsProviders, MetricsInterceptor],
 })
 export class MetricsModule {}
